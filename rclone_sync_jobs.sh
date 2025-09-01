21ba7f3868bc05d9f818295796bfe4c0eb40fb9b
Gotcha26 <spambiengentil@gmail.com>
2025-09-01 15:08:45 +0200

    On écarte les jobs problématiques

--- rclone_sync_jobs.sh
+++ rclone_sync_jobs.sh
@@ -18,9 +18,24 @@ NO_CHANGES_ALL=true
 PREVIOUS_JOB_PRESENT=false    # Variable pour savoir si un job précédent a été ajouté
 
 ###############################################################################
-# Exécution des jobs
+# Filtrer les jobs valides (remotes OK)
 ###############################################################################
-for job in "${JOBS_LIST[@]}"; do
+declare -a VALID_JOBS
+for idx in "${!JOBS_LIST[@]}"; do
+    # Vérifier si le job est marqué comme OK
+    if [[ "${JOBS_STATUS[$idx]}" == "OK" ]]; then
+        VALID_JOBS+=("${JOBS_LIST[$idx]}")
+    else
+        # Affichage info job écarté
+        print_fancy --theme "warning" "Job écarté : ${JOBS_LIST[$idx]} (remote inaccessible ou token expiré)"
+    fi
+done
+
+###############################################################################
+# Exécution des jobs filtrés
+###############################################################################
+PREVIOUS_JOB_PRESENT=false
+for job in "${VALID_JOBS[@]}"; do
     src="${job%%|*}"
     dst="${job##*|}"
     JOB_ID=$(printf "JOB%02d" "$JOB_COUNTER")    # Identifiant du job [JOB01], [JOB02], ...
@@ -34,8 +49,6 @@ for job in "${JOBS_LIST[@]}"; do
     TMP_JOB_LOG_HTML="$TMP_JOBS_DIR/${JOB_ID}_html.log"
     TMP_JOB_LOG_PLAIN="$TMP_JOBS_DIR/${JOB_ID}_plain.log"
 
-    # Affichage header job et redirection vers le log temporaire
-    # Affichage filtré vers le HTML pour supprimer les balises ANSI
     TMP_JOB_LOG_INFO="$(mktemp)"
     {
         print_fancy --align "center" "[$JOB_ID] $src → $dst"
@@ -43,7 +56,6 @@ for job in "${JOBS_LIST[@]}"; do
         echo ""
     } | tee -a "$LOG_FILE_INFO" | tee -a "$TMP_JOB_LOG_INFO"
 
-        # Header HTML + ligne vide
     {
         echo "<b>[$JOB_ID]</b> $src → $dst<br>"
         echo "$MSG_TASK_LAUNCH $NOW<br>"
@@ -54,39 +66,26 @@ for job in "${JOBS_LIST[@]}"; do
     rclone sync "$src" "$dst" "${RCLONE_OPTS[@]}" >> "$TMP_JOB_LOG_RAW" 2>&1 &
     RCLONE_PID=$!
 
-    # Afficher le spinner tant que rclone tourne
     spinner $RCLONE_PID
-
-    # Attendre fin rclone et récupérer code retour
     wait $RCLONE_PID
     job_rc=$?
     (( job_rc != 0 )) && ERROR_CODE=8
 
-    # Affichage colorisé après exécution dans la console
     colorize < "$TMP_JOB_LOG_RAW" | tee -a "$LOG_FILE_INFO"
-
-    # Génération logs HTML & plain
     prepare_mail_html "$TMP_JOB_LOG_RAW" >> "$TMP_JOB_LOG_HTML"
     make_plain_log "$TMP_JOB_LOG_RAW" "$TMP_JOB_LOG_PLAIN"
 
     # Assemblage HTML global
-    # Ajouter un séparateur seulement si ce n'est pas le premier job
     if $PREVIOUS_JOB_PRESENT; then
         GLOBAL_HTML_BLOCK+="<br><br><hr style='border:none; border-top:1px solid #ccc; margin:2em 0;'><br><br>"
     fi
     GLOBAL_HTML_BLOCK+=$(cat "$TMP_JOB_LOG_HTML")
     PREVIOUS_JOB_PRESENT=true
 
-    # On marque qu’un job a déjà été ajouté
-    PREVIOUS_JOB_PRESENT=true
-
-    # Notification Discord
     send_discord_notification "$TMP_JOB_LOG_PLAIN"
 
-    # Incrément du compteur pour le prochain job
     ((JOBS_COUNT++))
     (( job_rc != 0 )) && MAIL_SUBJECT_OK=false
     ((JOB_COUNTER++))
     echo
-
 done
