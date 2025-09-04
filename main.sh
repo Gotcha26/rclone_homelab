6ab9c994209bc51f6c74451813329d826b57aab3
Gotcha26 <spambiengentil@gmail.com>
2025-09-04 19:44:25 +0200

    Réorganisation des fichiers

--- main.sh
+++ main.sh
@@ -18,10 +18,6 @@ source "$SCRIPT_DIR/functions/init.sh"
 source "$SCRIPT_DIR/export/mail.sh"
 source "$SCRIPT_DIR/export/discord.sh"
 
-# --- DEBUG ---
-echo "DEBUG SCRIPT_DIR = $SCRIPT_DIR"
-# --- DEBUG ---
-
 # Initialisation de variables
 FORCE_UPDATE="false"
 UPDATE_TAG="false"
