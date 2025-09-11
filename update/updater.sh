1cfdd0964bb14072fa6a184efec866c3bc4816a5
Gotcha26 <spambiengentil@gmail.com>
2025-09-12 00:49:39 +0200

    ....

--- update/updater.sh
+++ update/updater.sh
@@ -111,56 +111,64 @@ analyze_update_status() {
         # Branche main : vérifier si on est à jour avec la dernière release
         if [[ -z "$latest_tag" ]]; then
             $do_display && print_fancy --fg "red" --bg "white" --style "bold underline" "$MSG_MAJ_ERROR"
-            return 1
-            git_summary $?
+            result_code=1
+            git_summary $result_code
+            return $result_code
         fi
 
         if [[ "$head_commit" == "$latest_tag_commit" ]] || git merge-base --is-ancestor "$latest_tag_commit" "$head_commit"; then
             $do_display && print_fancy "" || true
             $do_display && print_fancy --theme "success" "Version actuelle ${current_tag:-dev} >> A jour"
-            return 0
-            git_summary $?
+            result_code=0
+            git_summary $result_code
+            return $result_code
         fi
 
         if (( latest_tag_epoch < head_epoch )); then
             $do_display && print_fancy "" || true
             $do_display && print_fancy --theme "warning" --bg "yellow" --align "center" --highlight "Attention : votre commit local est plus récent que la dernière release !"
             $do_display && print_fancy --theme "follow" "Forcer la mise à jour pourrait écraser des changements locaux"
-            return 0
-            git_summary $?
+            result_code=0
+            git_summary $result_code
+            return $result_code
         else
             $do_display && print_fancy "" || true
             $do_display && print_fancy --theme "flash" --bg "blue" --align "center" --highlight "Nouvelle release disponible : $latest_tag ($(date -d "@$latest_tag_epoch"))"
             $do_display && print_fancy --theme "info" "Pour mettre à jour : relancer le script en mode menu ou utiliser --update-tag"
-            return 1
-            git_summary $?
+            result_code=1
+            git_summary $result_code
+            return $result_code
         fi
     else
         # Branche dev ou autre
         if [[ -z "$remote_commit" ]]; then
             $do_display && print_fancy "" || true
             $do_display && print_fancy --theme "info" "Aucune branche distante détectée pour '$branch_real'"
-            return 1
-            git_summary $?
+            result_code=1
+            git_summary $result_code
+            return $result_code
         fi
 
         if [[ "$head_commit" == "$remote_commit" ]]; then
             $do_display && print_fancy "" || true
             $do_display && print_fancy --theme "success" "Votre branche est à jour avec l'origine."
-            return 0
-            git_summary $?
+            result_code=0
+            git_summary $result_code
+            return $result_code
         fi
 
         if (( head_epoch < remote_epoch )); then
             $do_display && print_fancy "" || true
             $do_display && print_fancy --theme "flash" --bg "blue" --align "center" --highlight "Mise à jour disponible : votre commit est plus ancien que origin/$branch_real"
-            return 1
-            git_summary $?
+            result_code=1
+            git_summary $result_code
+            return $result_code
         else
             $do_display && print_fancy "" || true
             $do_display && print_fancy --theme "warning" --bg "green" --align "center" --highlight "Votre commit est plus récent que origin/$branch_real"
-            return 0
-            git_summary $?
+            result_code=0
+            git_summary $result_code
+            return $result_code
         fi
     fi
     $do_display && print_fancy --fill "#" ""
