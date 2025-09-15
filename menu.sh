3d2e8e2511d84db41ed9971fc4ad66420753338b
Gotcha26 <spambiengentil@gmail.com>
2025-09-15 12:54:17 +0200

    .

--- menu.sh
+++ menu.sh
@@ -9,8 +9,8 @@ SCRIPT_PATH="$(readlink -f "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
 # Sourcing global
-source "$SCRIPT_DIR/config/config.main.conf"
 source "$SCRIPT_DIR/config/global.conf"
+source "$SCRIPT_DIR/config/config.main.conf"
 source "$SCRIPT_DIR/functions/dependances.sh"
 source "$SCRIPT_DIR/functions/core.sh"
 source "$SCRIPT_DIR/update/updater.sh"
