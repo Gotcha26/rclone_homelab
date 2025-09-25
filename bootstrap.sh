dad259ee458f9f63096cfb35878ee76bda3b7fc4
Gotcha26 <spambiengentil@gmail.com>
2025-09-25 16:46:35 +0200

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -143,7 +143,6 @@ update_local_configs() {
         return 2
     else
         display_msg "soft|verbose|hard" --theme info "Aucun changement détecté sur les fichiers d'exemples."
-        echo "[DEBUG] test"
         return 0
     fi
 
